cc543e455be2564fd30134671d5ce220c471aa34
RSMarian <marianstefan1499@gmail.com>
2022-06-04 20:32:27 +0300

    added functionality by traversing the DOM

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-//using selectors inside the element
+// using selectors inside the element
 // traversing the dom
+
+// select all the buttons
+// loop over the buttons
+// add event listeners
+// make sure the parent is the question
+// add .show-text to the parent
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e){
+        const question = e.currentTarget.parentElement.parentElement;
+        question.classList.toggle('show-text');
+    });
+});
